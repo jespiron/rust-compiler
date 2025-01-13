c3978b761013a6fd2f090aeff72cd037c6194942
Jessica Ruan <jessica.ruan@outlook.com>
2025-01-12 21:56:25 -0500

    begin AST

--- src/parser.rs
+++ src/parser.rs
@@ -1,9 +1,32 @@
 // Parser: tokens -> ast
-
 use crate::lexer::Token;
 
+// Program is comprised of variables and functions
+pub struct Program {
+    pub decl: Vec<VarDecalaration>,
+    pub fns: Vec<FnDeclaration>,
+}
+
+// Example: `const int my_variable = !(2+3)`
+pub struct VarDeclaration {
+    is_const: bool,    // true
+    _type: Token,      // `int`
+    identifier: Token, // `my_variable`
+    value: Expr,       // Unary(Bang, Parentheses(Binary(Number(2.0), Plus, Number(2.0))))
+}
+
+// TODO
+pub struct FnDeclaration {
+    return_type: Token,
+}
+
 #[derive(Debug)]
-pub struct AstNode {}
+pub enum Expr {
+    Literal(Token),            // leaf node of the expression tree
+    Unary(Token, Expr),        // like `!expression`
+    Binary(Expr, Token, Expr), // like `2+3`
+    Parentheses(Expr),         // like `(expression)`
+}
 
 pub fn parse(tokens: Vec<Token>) -> AstNode {
     AstNode {}
